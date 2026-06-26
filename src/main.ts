8d476059b5898de9c31129c4361b2b65ab76e7fa
Turatbek Masiraliev <turatbek005@gmail.com>
2026-06-26 22:19:32 +0600

    Настройка отображения HTML

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'node:path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('ejs');
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`[NestJS] Сервер успешно запущен на порту: ${port}`);
+  console.log(
+    `[NestJS] Админ-панель будет доступна по адресу: http://localhost:${port}/admin`,
+  );
 }
-bootstrap();
+void bootstrap();
